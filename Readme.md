7a817a0091d8ebb47c203268737b14cde5a12442
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2019-03-05 22:01:18 -0300

    commit inicial para template html.

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## Templates base para front-end.
---
+++ html/js/script.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    document.getElementById("go").style.width = "50%";
+    document.getElementById("go").style.height = "40%";
+}
+
+function closeMenu() {
+    document.getElementById("go").style.width = "0";
+    document.getElementById("go").style.height = "0";
+
+}       
